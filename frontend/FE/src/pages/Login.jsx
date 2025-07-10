ea1c63fdfd32136998467d05770f50e25b986f6b
rishika-2626 <rishikasruthi@gmail.com>
2025-07-10 23:36:05 +0530

    login and payment

--- frontend/FE/src/pages/Login.jsx
+++ frontend/FE/src/pages/Login.jsx
@@ -1,10 +1,29 @@
 function Login(){
     return(
-        <div>
+
+       <div className="page">
+         <div className='main'>
+
+             <div className="signUp" > Sign Up</div>
+
+            <div className="input">
+                <input className="username" placeholder="Enter username" type="text"></input>
+            </div>
+
+            <div className='input'>
+                <input className="email" placeholder="Enter email" type="email"></input>
+            </div>
+
+            <div className='input'>
+                <input className="password" placeholder="Enter password" type="password"></input>
+            </div>
             
-           <h1 style={{color:"white"}}> login page</h1>
-            
+            <div className="btn">
+                <button className="getStarted">get started</button>  
+           </div>
+
         </div>
+       </div>
 
     );
 };
