546fa40beca5c49760a825352b296a0caf54c77e
rishika-2626 <rishikasruthi@gmail.com>
2025-07-13 11:51:30 +0530

    backend

---
+++ BE/backend/src/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const movieRoutes = require('./routes/movieRoutes');
+const theatreRoutes = require('./routes/theatreRoutes');
+const showtimeRoutes = require('./routes/showtimeRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+app.use(express.json()); // this makes it possible for your express server to understand and work with json data sent by clients
+
+app.use('/api/movies', movieRoutes);
+app.use('/api/theatres', theatreRoutes);
+app.use('/api/showtimes', showtimeRoutes);
+app.use('/api/bookings', bookingRoutes);
+app.use('/api/auth', authRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('DB connected'))
+.catch(err => console.error(err));
+
+app.listen(5000, () => console.log('Server running on port 5000'));
